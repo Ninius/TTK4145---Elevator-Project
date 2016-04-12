d12c28b265901bf944d655047dc6f9762dec7d30
Øystein Helgesen <oysteikh@stud.ntnu.no>
2016-04-12 14:30:52 +0200

    Switched to Atomic types in OperationManager when needed for non-blocking Thread safety

--- src/com/gruppe78/OperativeManager.java
+++ src/com/gruppe78/OperativeManager.java
@@ -3,15 +3,19 @@ package com.gruppe78;
 import com.gruppe78.model.Floor;
 import com.gruppe78.model.SystemData;
 
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicLong;
+
 /**
  * Created by oysteikh on 4/12/16.
  */
 
 //Implement as listener?
 public class OperativeManager {
-    long lastEventTime;
+    AtomicLong lastEventTime;
     private static OperativeManager sOperativeManager;
     private Thread managerThread;
+    private AtomicBoolean active;
 
     private OperativeManager(){
         lastEventTime = System.currentTimeMillis();
@@ -23,18 +27,21 @@ public class OperativeManager {
         return sOperativeManager;
     }
     public void newEvent(){
-        lastEventTime = System.currentTimeMillis();
+        lastEventTime.set(System.currentTimeMillis());
     }
     public void start(){
         managerThread = new ManagerThread();
         managerThread.setName(ManagerThread.class.getSimpleName());
         managerThread.start();
     }
+    public void setActive(boolean Active){
+        active.set(Active);
+    }
     private class ManagerThread extends Thread{
         @Override public void run(){
             try{
                 while (true){
-                    if (System.currentTimeMillis()-lastEventTime > 4000){
+                    if (System.currentTimeMillis()-lastEventTime.get() > 4000 && active.get()){
                         SystemData.get().getLocalElevator().setOperable(false);
                     }
                     else{
